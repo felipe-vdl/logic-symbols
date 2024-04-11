aa3ae092018d4c7b17d01072743921309e3d7488
Felipe Vidal <vidalfelipe2018@gmail.com>
2024-04-10 21:00:22 -0300

    ui, components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          1: "#161616",
+          2: "#282828",
+          3: "#323232",
+          4: "#333333",
+        },
+        light: {
+          1: "#FAFAFA",
+          2: "#D2D2D2",
+          3: "#C3C3C3",
+          4: "#7d7d7d",
+        },
+        accent: "#346899",
+      },
+    },
+  },
+  plugins: [],
+};
